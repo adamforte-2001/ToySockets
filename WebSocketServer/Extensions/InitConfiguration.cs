a42269a26f1b73063c9a6129eb5a64d57e87664e
aforte-roomstogo <aforte@roomstogo.com>
2025-10-21 13:40:42 -0400

    Add managed Azure SignalR instance functionality

---
+++ WebSocketServer/Extensions/InitConfiguration.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace WebSocketServer.Extensions
+{
+    public static class InitConfiguration
+    {
+        private const string LOCAL_APP_SETTINGS_FILE = "appsettings.local.json";
+        public static WebApplicationBuilder AddConfiguration(this WebApplicationBuilder builder, ILogger logger)
+        {
+            if (!File.Exists(LOCAL_APP_SETTINGS_FILE))
+            {
+                throw new FileNotFoundException($"Error - {LOCAL_APP_SETTINGS_FILE} must be present");
+            }
+            try
+            {
+                builder
+                    .Configuration
+                    .SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile(LOCAL_APP_SETTINGS_FILE, optional: false, reloadOnChange: true);
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, "Failed to load configuration");
+                throw;
+            }
+            return builder;
+        }
+    }
+}
--- WebSocketServer/Program.cs
+++ WebSocketServer/Program.cs
@@ -2,18 +2,35 @@
 using Microsoft.Extensions.DependencyInjection;
 using WebSocketServer.Extensions;
 using WebSocketServer.Services;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using Microsoft.Azure.SignalR;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddSignalR();
+using var loggerFactory = LoggerFactory.Create(loggingBuilder => loggingBuilder
+                                        .SetMinimumLevel(LogLevel.Trace)
+                                        .AddConsole());
+var startupLogger = loggerFactory.CreateLogger<Program>();
+builder.AddConfiguration(startupLogger);
+
+var useAzureSignalR = builder.Configuration.GetValue<bool>("FeatureManagement:USE_AZURE_SIGNALR");
+var signalRServerBuilder = builder.Services.AddSignalR();
+if (useAzureSignalR)
+{
+    var AzureSignalRConnectionString = builder.Configuration.GetValue<string>("ConnectionStrings:SignalR");
+    signalRServerBuilder.AddAzureSignalR(AzureSignalRConnectionString);
+}
+
+var frontendServer = builder.Configuration.GetValue<string>("FrontendServer:Server");
+var frontendPort = builder.Configuration.GetValue<string>("FrontendServer:Port");
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowDev", builder =>
     {
-        builder.WithOrigins("https://localhost:3000") // Replace with your allowed origins
+        builder.WithOrigins($"https://{frontendServer}:{frontendPort}") 
             .AllowAnyHeader()
-            .AllowAnyMethod() // Or AllowAnyMethod() for all
-            .AllowCredentials(); // Important for SignalR authentication
+            .AllowAnyMethod() 
+            .AllowCredentials(); 
     });
 });
 var app = builder.Build();
